e22a06aea7450b8c0bdebcd800234cfe6e6dfe2e
nazish-qamar <nazishqamar01@gmail.com>
2023-06-13 12:57:36 +0200

    rnn, lstm and gru

--- rnn.py
+++ rnn.py
@@ -1,117 +1,121 @@
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
+import torchvision
+import torchvision.transforms as transforms
 
-from utils import ALL_LETTERS, N_LETTERS, line_to_tensor
-from utils import load_data, letter_to_index, letter_to_tensor, random_training_example
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# hyper parameters
+input_size = 28
+sequence_length = 28
+hidden_size = 128
+num_layers = 2  # by default, it will be 1
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
+                                           transform=transforms.ToTensor(), download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False,
+                                          transform=transforms.ToTensor(), download=True)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
 
 
 class RNN(nn.Module):
-    # nn.RNN
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
         super(RNN, self).__init__()
-
+        self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
-        self.i2o = nn.Linear(input_size + hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)  # input is of shape (1, 57)
 
-    def forward(self, input_tensor, hidden_tensor):
-        combined = torch.cat((input_tensor, hidden_tensor), 1)
-        hidden = self.i2h(combined)
-        output = self.i2o(combined)
-        output = self.softmax(output)
-        return output, hidden
+        ######## Model Specific Change ##########
+        # For RNN
+        #self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
 
-    def init_hidden(self):
-        return torch.zeros(1, self.hidden_size)  # for initialization of hidden tensors
+        # For GRU
+        #self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
 
+        # For LSTM
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
 
-category_lines, all_categories = load_data()
-n_categories = len(all_categories)
+        #########################################
+        # x -> (batch_size, seq, input_size)
+        self.fc = nn.Linear(hidden_size, num_classes)
 
-n_hidden = 128
-rnn = RNN(N_LETTERS, n_hidden, n_categories)
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
 
-# testing with sequence (name)
-input_tensor = line_to_tensor("Albert")
-hidden_tensor = rnn.init_hidden()
+        ########### Model Specific Change ###########
+        # For RNN
+        #out, _ = self.rnn(x, h0)
 
-output, next_hidden = rnn(input_tensor[0], hidden_tensor)
-print(output.size())
-print(next_hidden.size())
+        # For GRU
+        #out, _ = self.gru(x, h0)
 
+        # For LSTM
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (h0, c0))
+        #############################################
 
-def category_from_output(output):
-    category_index = torch.argmax(output).item()
-    return all_categories[category_index]
-
-
-print(category_from_output(output))
-
-criterion = nn.NLLLoss()
-learning_rate = 0.005
-optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+        # out_shape: batch_size, sequence_len, hidden_size
+        # out (N, 28, 128)
+        out = out[:, -1, :]
+        # out (N, 128)
+        out = self.fc(out)
+        return out
 
 
-def train(line_tensor, category_tensor):
-    hidden = rnn.init_hidden()
+model = RNN(input_size, hidden_size, num_layers, num_classes)
+model = model.to(device)
 
-    for i in range(line_tensor.size()[0]):
-        output, hidden = rnn(line_tensor[i], hidden)
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    loss = criterion(output, category_tensor)
+# training loop
+n_total_steps = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        # Reshape tensor (100, 1, 28, 28) -> (100, 28, 28)
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        # forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
 
-    return output, loss.item()
+        # backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-
-current_loss = 0
-all_losses = []
-plot_steps, print_steps = 1000, 5000
-n_iters = 100000
-
-for i in range(n_iters):
-    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
-
-    output, loss = train(line_tensor, category_tensor)
-    current_loss += loss
-
-    if (i + 1) % plot_steps == 0:
-        all_losses.append(current_loss / plot_steps)
-        current_loss = 0
-
-    if (i + 1) % print_steps == 0:
-        guess = category_from_output(output)
-        correct = "CORRECT" if guess == category else f"WRONG ({category})"
-        print(f"{i} {i/n_iters * 100} {loss:.4f} {line} / {guess} {correct}")
+        if (i+1) % 100 == 0:
+            print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
 
 
-plt.figure()
-plt.plot(all_losses)
-plt.show()
+# test
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
 
+        # value, index
+        _, predictions = torch.max(outputs, 1)
+        n_samples += labels.shape[0]
+        n_correct += (predictions == labels).sum().item()
 
-def predict(input_line):
-    print(f"\n {input_line}")
-    with torch.no_grad():
-        line_tensor = line_to_tensor(input_line)
-
-        hidden = rnn.init_hidden()
-
-        for i in range(line_tensor.size()[0]):
-            output, hidden = rnn(line_tensor[i], hidden)
-
-        guess = category_from_output(output)
-        print(guess)
-
-
-while True:
-    sentence = input("Input:")
-    if sentence == "quit":
-        break
-
-    predict(sentence)
+    acc = 100.0 * n_correct / n_samples
+    print(f'accuracy {acc }')
